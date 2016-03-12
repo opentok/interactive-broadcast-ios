b33b4363cd53ed760b4543f48c6daa70c88e66bd
Lucas Huang <lucas@tokbox.com>
2016-03-11 16:02:08 -0800

    remove SpotlightApi and dataButton

--- SpotlightKit/SpotlightKit.h
+++ SpotlightKit/SpotlightKit.h
@@ -16,9 +16,6 @@ FOUNDATION_EXPORT const unsigned char SpotlightKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SpotlightKit/PublicHeader.h>
 
-#import <SpotlightKit/SpotlightApi.h>
 #import <SpotlightKit/EventViewController.h>
 #import <SpotlightKit/EventsViewController.h>
 #import <SpotlightKit/MainSpotlightControllerViewController.h>
-#import <SpotlightKit/dataButton.h>
-

4a67cbdb7fa21225a24e302bc1f30306147375df
Lucas Huang <lucas@tokbox.com>
2016-05-25 09:43:17 -0700

    CocoaPods setup

---
+++ IBKit/Events/EventCell.h
@@ -0,0 +1,13 @@
+//
+//  EventCell.h
+//  IBDemo
+//
+//  Created by Xi Huang on 5/25/16.
+//  Copyright © 2016 Tokbox, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventCell : UICollectionViewCell
+
+@end
---
+++ IBKit/Events/EventCell.m
@@ -0,0 +1,13 @@
+//
+//  EventCell.m
+//  IBDemo
+//
+//  Created by Xi Huang on 5/25/16.
+//  Copyright © 2016 Tokbox, Inc. All rights reserved.
+//
+
+#import "EventCell.h"
+
+@implementation EventCell
+
+@end

c5dfa8c240fd2435d8e477569f7115378b119491
Andrea Phillips <andrea@agilityfeat.com>
2016-06-02 17:42:59 -0600

    changed AppUtil tests description and added one more test

--- IBKitTests/AppUtilTests.m
+++ IBKitTests/AppUtilTests.m
@@ -15,29 +15,33 @@ SPEC_BEGIN(AppUtilTests)
 context(@"UtilTests", ^(){
     
     describe(@"Converts a status to string", ^(){
-        it(@"should return Live status", ^(){
+        it(@"returns Live is the status is L", ^(){
             IBEvent *event = [[IBEvent alloc] initWithJson:@{@"status": @"L"}];
             [[[AppUtil convertToStatusString:event] should] equal:@"Live"];
         });
         
-        it(@"should return Not Started", ^(){
+        it(@"returns Not Started if event status is P", ^(){
             IBEvent *event = [[IBEvent alloc] initWithJson:@{@"status": @"P"}];
             [[[AppUtil convertToStatusString:event] should] equal:@"Not Started"];
         });
         
-        it(@"should return Closed", ^(){
+        it(@"should returns Closed if the event status is C", ^(){
             IBEvent *event = [[IBEvent alloc] initWithJson:@{@"status": @"C"}];
             [[[AppUtil convertToStatusString:event] should] equal:@"Closed"];
         });
         
-        it(@"should return Not Started", ^(){
+        it(@"should return Not Started if the event status is N", ^(){
             IBEvent *event = [[IBEvent alloc] initWithJson:@{@"status": @"N"}];
             [[[AppUtil convertToStatusString:event] should] equal:@"Not Started"];
         });
         
-        it(@"should return a formated date", ^(){
+        it(@"returns a correctly formatted date if there is a correctly formatted startTime", ^(){
             IBEvent *event = [[IBEvent alloc] initWithJson:@{@"status": @"N", @"date_time_start":@"2016-02-14 11:11:11"}];
             [[[AppUtil convertToStatusString:event] should] equal:@"14 Feb 2016 11:11:11"];
+        });
+        it(@"returns nil if event status is not present", ^(){
+            IBEvent *event = [[IBEvent alloc] initWithJson:@{@"date_time_start":@"2016-02-14 11:11:11"}];
+            [[[AppUtil convertToStatusString:event] should] beNil];
         });
         
     });

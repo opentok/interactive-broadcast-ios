141127c036a9a20cf97dff3ac77098b68e8c6dc9
Lucas Huang <xhuang0802@gmail.com>
2017-03-08 12:06:10 -0800

    update script for S3 distribution (#123)
    * update script for S3 distribution
    
    * include pod update

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ WORKSPACE_NAME="IBDemo.xcworkspace"
 
 #Create zip file with binary and doc
 if [ "$task" == "-d" ]; then
-        xcodebuild clean test -workspace "${WORKSPACE_NAME}" -scheme "IBKitTests" -sdk "iphonesimulator9.3" -destination "OS=9.3,name=iPhone 6 Plus" -configuration Debug 
+        pod update
+        xcodebuild clean test -workspace "${WORKSPACE_NAME}" -scheme "IBKitTests" -sdk "iphonesimulator10.0" -destination "OS=10.0,name=iPhone 6 Plus" -configuration Debug
         if [${PIPESTATUS[0]} == true]; then
         	exit ${PIPESTATUS[0]}
         fi
